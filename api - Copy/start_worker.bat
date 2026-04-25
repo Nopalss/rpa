4f89f231d30eacf6ea3184064dacd5daaf005a50
nopalss <holanakdo@gmail.com>
2026-04-25 20:27:27 +0700

    hapus

--- api - Copy/start_worker.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title RPA Redis Worker Controller
-
-cd /d D:\laragon\www\rpa\api
-
-powershell -NoProfile -ExecutionPolicy Bypass -File "D:\laragon\www\rpa\api\worker_controller.ps1"
-
-pause
